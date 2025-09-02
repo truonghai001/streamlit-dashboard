2476f65b53cdc792b52e6121d171bb1de88503f4
Hai Truong <haitruong3001@gmail.com>
2025-09-02 11:50:51 +0700

    Read iris dataset and display first 5 samples

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import pandas as pd
+
 st.title('Hello, Streamlit Dashboard!')
+
+# Load dataset
+IRIS_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+COLUMN_NAMES = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+df = pd.read_csv(IRIS_URL, names=COLUMN_NAMES)
+
+# View dataset
+st.header("Step 1: Iris Dataset Loaded!")
+st.write("Here is what the data look like:")
+st.write(df.head())
