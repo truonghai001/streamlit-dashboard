352e4773122b50f0ae4bed7aef8e5d08937ce50b
Hai Truong <haitruong3001@gmail.com>
2025-09-03 18:30:36 +0700

    add download feature

--- pages/churn_app.py
+++ pages/churn_app.py
@@ -26,5 +26,16 @@ customer churn means the customer does not make another purchase after a period 
 df_selected = pd.read_csv('./ml_models/data/telco_churn.csv')
 df_selected_all = df_selected[['gender', 'Partner', 'Dependents', 'PhoneService','tenure', 'MonthlyCharges', 'Churn']].copy()
 
-st.dataframe(df_selected)
+st.dataframe(df_selected_all)
 
+# define function to let user download data file
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64} download="churn_data.csv">Download CSV File</a>'
+
+    return href
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.markdown(filedownload(df_selected_all), unsafe_allow_html=True)
