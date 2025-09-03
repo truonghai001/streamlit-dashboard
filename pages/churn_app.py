1791a3c86a01ddf1c308f7f8b4b5faa752ad067a
Hai Truong <haitruong3001@gmail.com>
2025-09-03 18:22:21 +0700

    correct path

--- pages/churn_app.py
+++ pages/churn_app.py
@@ -22,7 +22,7 @@ customer churn means the customer does not make another purchase after a period 
 
 """)
 
-df_selected = pd.read_csv('./ml_module/data/telco_churn.csv')
+df_selected = pd.read_csv('../ml_module/data/telco_churn.csv')
 df_selected_all = df_selected[['gender', 'Partner', 'Dependents', 'PhoneService','tenure', 'MonthlyCharges', 'target']].copy()
 
 st.dataframe(df_selected)
